c645796f9adc720f57ab93a26a3c345a56211e4c
Louis Charette <charette.louis@gmail.com>
2014-04-01 22:41:42 -0400

    Added background detection
    Added detection of aped instances running the background and a dialog
    to warn the user a new instances can’t be started.
    
    Added app icon.

--- APE Server Controler/apeAppDelegate.m
+++ APE Server Controler/apeAppDelegate.m
@@ -57,10 +57,22 @@
 #pragma mark - UI Elements Actions
 
 - (IBAction)startServer:(id)sender {
+    
+    BOOL ApeRunning = [self isApeAlreadyRunning];
 
     //Start only if not already running
-    if (![self.APEtask isRunning]) {
-        [self defineNSTask]; //We need to redefine the task everytime we launch it, so...
+    if (![self.APEtask isRunning] && !ApeRunning) {
+        
+        //We need to redefine the task everytime we launch it, so, we do this
+        //to call a new server instance.
+        [self defineNSTask];
+        
+    } else if (ApeRunning) {
+        
+        //Ape is running in the background (ie. not withing this app)
+        //Warn the user about that
+        [self warnApeAlredyRunning];
+        
     }
 }
 - (IBAction)stopServer:(id)sender {
@@ -97,23 +109,105 @@
 # pragma mark - Utility Method
 
 - (BOOL)isApeInstalled {
+    
     //TODO...
+    
     return true;
 }
 
-- (void)checkIfApeAlreadyRunning {
+- (BOOL)isApeAlreadyRunning {
     
-    //Check if an APE instance is already running in background using other method
-    //We do this using a bundled sh script and/or NSTask and "ps".
-    //If it is running, warn user with choice of killing the background one and start a new one here it or leave it
-    //running and abord launching a new one
-    //NSTASK --> "pgrep aped"
+    //Check if an APE instance is already running in background using other method.
+    //We do this using a bundled an NSTask and "pgrep" command.
     
     //We could also detect other background running process and manage it using switchRunningState and the stop server
     //for transparency purposes.
     
+    //TODO: We could test to see if if the running instance is on the same port
+    //      as we're trying to launch. Multiple Ape Instances can work at the same time on different port
+    
+    //Define a separate task
+    NSTask *ApeCheck = [[NSTask alloc] init];
+    
+    //We don't need to worry for UI freeze here
+    @try {
+        
+        //Path to aped
+        NSString *path  = [NSString stringWithFormat:@"/usr/bin/pgrep"];
+        
+        //Arguments
+        NSMutableArray *arguments = [[NSMutableArray alloc] init];
+        [arguments addObject:@"aped"];
+        
+        //Set up the task
+        ApeCheck.launchPath = path;
+        ApeCheck.arguments  = arguments;
+        
+        //Set up an output pipe to catch the answer
+        [ApeCheck setStandardOutput: [NSPipe pipe]];
+        
+        //Start the task
+        [ApeCheck launch];
+        
+        //Wait until we have our answer
+        [ApeCheck waitUntilExit];
+        
+        //Put the outpout value into a string
+        NSData *taskData = [[[ApeCheck standardOutput] fileHandleForReading] availableData];
+        NSString *taskDataString = [[NSString alloc] initWithData: taskData encoding: NSUTF8StringEncoding];
+        
+        if ([taskDataString isEqualToString:@""]) {
+            
+            //Ape currently not running
+            return false;
+            
+        } else {
+            
+            //DEBUG:
+            NSLog(@"Ape already running on PID = %@", taskDataString);
+            
+            //Ape is indeed running
+            return true;
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Problem Running Ape Check Task: %@", [exception description]);
+        return false;
+    }
+}
+
+-(void) warnApeAlredyRunning
+{
+    //If Ape is running, the user is warned with choice of killing the background one and start a new one here it or leave it
+    //running and abord launching a new one
+    
+    //TODO: Prompt for action and kill the process
+    
+    //Setup the dialog...
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"OK"];
+    //[alert addButtonWithTitle:@"Cancel"];
+    [alert setMessageText:@"Ape Server already running"];
+    [alert setInformativeText:@"Another instance of Ape Server seems to be already running in the background. Can't launch a new instance while another process is already running"];
+    [alert setAlertStyle:NSWarningAlertStyle];
+    
+    
+    //Temp:
+    [alert runModal];
+    
+    //Display warning and test the result
+    //if ([alert runModal] == NSAlertFirstButtonReturn) {
+        // OK clicked,...
+        // [self killRunningApes];
+        // [self startServer];
+    //}
+}
+
+-(void) killRunningApes
+{
+    //Kill running apes using "pkill aped".
+    //This need to be run as root
     //TODO...
-    //return false;
 }
 
 #pragma mark - NSTask management
